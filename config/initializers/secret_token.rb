11f64661ac590495445c0d29ffaa775901034e16
Kai Rollmann <kai.rollmann@googlemail.com>
2013-01-13 22:01:54 +0100

    Add a basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Progress::Application.config.secret_token = '941bafc963ef1f3aad2abbfd6d4c30e1df1a5287f30dc4ee938c5ea14ba98bc6f083fa52818eb55a333300ad34844344feaa082804d65c359f826d4ad6405a0c'
